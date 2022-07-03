a484c52ba415c2f70299d6f03784356d39165058
swdrana <tctr2s@gmail.com>
2022-07-03 19:05:02 +0600

    daisyUI installed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div><button class="btn">Button</button>
+    <button class="btn btn-primary">Button</button>
+    <button class="btn btn-secondary">Button</button>
+    <button class="btn btn-accent">Button</button>
+    <button class="btn btn-ghost">Button</button>
+    <button class="btn btn-link">Button</button>
     </div>
   );
 }
